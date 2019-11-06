052cd1b3a767255e314b8e68e8d2cd7db5a907d4
Andrew Pond <apond@pop-os.localdomain>
2019-11-05 19:03:43 -0700

    changes

--- lab3/Database.cpp
+++ lab3/Database.cpp
@@ -6,15 +6,22 @@ void Database::createRelations(vector<Predicate> scheme_list,
                     vector<Rule> rule_list,
                     vector<Predicate> query_list)
 {
-    for (int x=0;x<scheme_list.size();x++)
+    for (auto current_scheme : scheme_list)
     {
         Scheme new_scheme = Scheme();
-        for (int y=0;y<scheme_list[x].name.size();y++){
-            new_scheme.push_back(scheme_list[x].name.substr(y, 1));
+        for (int y=0;y<current_scheme.name.size();y++){
+            new_scheme.push_back(current_scheme.name.substr(y, 1));
         }
 
-        Relation new_relation = Relation(scheme_list[x].name, new_scheme);
-        new_relation.addTouple(scheme_list[x].getParameters());
+        Relation new_relation = Relation(current_scheme.name, new_scheme);
+        
+
+        // Populate tuples
+        for(auto current_fact : fact_list)
+        {
+            if (current_fact.name == current_scheme.name)
+                new_relation.addTouple(current_fact.getParameters());
+        }
         
         this->insert(pair<string, Relation>(new_relation.name, new_relation));
     }
